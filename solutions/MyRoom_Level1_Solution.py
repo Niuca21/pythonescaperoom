dd7018e5cd3e43334bf3c943a5bdcb553d2e2db2
Lukasz Kotula <Lukasz.Kotula@getraenke-hoffmann.de>
2025-10-31 20:20:54 +0100

    Test Solutions delete

--- solutions/MyRoom_Level1_Solution.py
+++
@@ -1,6 +0,0 @@
-def run(letters):
-    numberstring = ""
-    for c in letters:
-        numberstring = numberstring + str(ord(c))
-    return numberstring
-    # return 33  # just to test wrong results
--- solutions/alte_Versionen/Level1Veronika.py
+++
@@ -1,2 +0,0 @@
-def run(cockie):
-    return "".join(chr(int(n)) for n in cockie.split())
